e0757c0d2603166ee38c2fb830473eb754e9e9b5
Roman <61010440+Aettio@users.noreply.github.com>
2021-03-17 16:16:39 +0200

    Update Code_Clustering.py

--- Code_Clustering.py
+++ Code_Clustering.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import plotly.express as px
 
+df = pd.read_csv("StudentsPerformance.csv")
+
+df.head()
+df.describe()
+
+# 3D Визуал
+
+fig = px.scatter_3d(df, x = "writing score", y='math score', z='reading score', color = 'gender')
+fig.show()
